feebf3a3b57d9a8a7a68d6cbb7906208067491b6
zhutx <passwd_1986>
2017-12-07 11:02:07 +0800

    提交修改

--- zhufresh-service/src/main/java/com/moredian/zhufresh/mapper/UserMapper.java
+++ zhufresh-service/src/main/java/com/moredian/zhufresh/mapper/UserMapper.java
@@ -2,10 +2,13 @@ package com.moredian.zhufresh.mapper;
 
 import com.moredian.zhufresh.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
 
     int insert(User user);
+
+    User loadByMobileLogin(@Param("mobile") String mobile, @Param("password") String password);
 
 }
