5d55483e634e085b97e2debe81212c3e8f597cb8
zhutx <passwd_1986>
2017-12-07 16:13:37 +0800

    提交修改

--- zhufresh-web/src/main/java/com/moredian/zhufresh/config/HttpSessionConfig.java
+++ zhufresh-web/src/main/java/com/moredian/zhufresh/config/HttpSessionConfig.java
@@ -6,19 +6,25 @@ import org.springframework.session.web.http.CookieHttpSessionStrategy;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
 
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = Integer.MAX_VALUE)
 public class HttpSessionConfig {
-
+    /**
+     * 以独立的header属性x-auth-token，返回sessionid的值。对接硬件设备可以采用这种方式。
+     */
+    /*
     @Bean
     public HttpSessionStrategy httpSessionStrategy() {
         return new HeaderHttpSessionStrategy();
     }
+    */
 
-    /*
+    /**
+     * 用cookie返回sessionid值。对接网页，可以使用这种方式。
+     * @return
+     */
     @Bean
     public HttpSessionStrategy httpSessionStrategy() {
         return new CookieHttpSessionStrategy();
     }
-    */
 
 }
